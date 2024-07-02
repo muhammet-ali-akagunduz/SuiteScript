d8c8dd81dc3f3ae9534a1566589eefc63ad658ce
muhammet-ali-akagunduz <muhammetaliakagunduz@gmail.com>
2024-07-02 17:04:19 +0300

    thirthcommit

--- src/FileCabinet/SuiteScripts/maa_sdr_ue_employee_1.js
+++ src/FileCabinet/SuiteScripts/maa_sdr_ue_employee_1.js
@@ -4,13 +4,23 @@
  * @NScriptType UserEventScript
  * 
  */
-define([],
-    function () {
+define(['N/record', 'N/http'],
+    function (record) {
 
         return {
-            beforeLoad:function(context){},
 
             afterSubmit: function (context) {
+                var employee = context.newRecord;
+
+                var supervisor = record.load({
+                    type: record.Type.EMPLOYEE,
+                    id: employee.getValue('supervisor')
+                });
+
+                log.debug('Emp Name', employee.getValue('entityid'));
+                log.debug('Supervisor ID', employee.getValue('supervisor'));
+                log.debug('Supervisor Name', employee.getText('supervisor'));
+                log.debug('Supervisor Email', supervisor.getValue('email'));
 
             }
         };
